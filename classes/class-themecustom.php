9b472971c7a9705e7947585969fc83c352231f27
nico_martin <mail@nicomartin.ch>
2018-06-10 23:24:37 +0200

    added humens.txt

--- classes/class-themecustom.php
+++ classes/class-themecustom.php
@@ -10,6 +10,7 @@ class ThemeCustom {
 
 	public function run() {
 		add_action( 'after_setup_theme', [ $this, 'register_nav' ] );
+		add_action( 'wp_head', [ $this, 'humans_txt' ] );
 
 		// Third party plugin support
 		add_filter( 'hellolog_types', [ $this, 'register_error_log' ] );
@@ -20,6 +21,10 @@ class ThemeCustom {
 			'primary' => __( 'Primary Menu', 'sha' ),
 			'footer'  => __( 'Footer Menu', 'sha' ),
 		] );
+	}
+
+	public function humans_txt() {
+		echo '<link type="text/plain" rel="author" href="' . trailingslashit( get_template_directory_uri() ) . 'humans.txt" />';
 	}
 
 	public function register_error_log( $types ) {
