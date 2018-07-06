c6f5232c1909f47897a1441d3938ab61459129a5
nico_martin <mail@nicomartin.ch>
2018-07-06 23:34:54 +0200

    removed test images

--- classes/autoload/class-lazysizes.php
+++ classes/autoload/class-lazysizes.php
@@ -38,11 +38,11 @@ class Lazysizes {
 
 	public function svg_filter() {
 		?>
-		<svg class='lazysizes-svgfilter'>
-			<filter id='ls-sharp-blur'>
-				<feGaussianBlur stdDeviation='15'></feGaussianBlur>
-				<feColorMatrix type='matrix' values='1 0 0 0 0, 0 1 0 0 0, 0 0 1 0 0, 0 0 0 9 0'></feColorMatrix>
-				<feComposite in2='SourceGraphic' operator='in'></feComposite>
+		<svg class="lazysizes-svgfilter">
+			<filter id="ls-sharp-blur">
+				<feGaussianBlur stdDeviation="10"></feGaussianBlur>
+				<feColorMatrix type="matrix" values="1 0 0 0 0, 0 1 0 0 0, 0 0 1 0 0, 0 0 0 9 0"></feColorMatrix>
+				<feComposite in2="SourceGraphic" operator='in'></feComposite>
 			</filter>
 		</svg>
 		<?php
