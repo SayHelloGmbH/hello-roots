1612966eeba3d7de01e82803e42a9b126472f0f0
nico_martin <mail@nicomartin.ch>
2018-08-02 08:30:30 +0200

    Gulp - fix to high CPU usage: http://blog.murindwaz.com/gulp-high-cpu-usage/

--- .build/gulp/task-watch.js
+++ .build/gulp/task-watch.js
@@ -7,11 +7,11 @@ module.exports = function (key, config, gulp, $, errorLog) {
 
 		$.livereload.listen();
 
-		gulp.watch(mainConfig.styles.args.src, ['styles']);
+		gulp.watch(mainConfig.styles.args.src, {interval: 500}, ['styles']);
 		for (let subtask of mainConfig.scripts.subtasks) {
-			gulp.watch(`${mainConfig.scripts.args.build}${subtask}/**/*.js`, [`scripts:${subtask}`]);
+			gulp.watch(`${mainConfig.scripts.args.build}${subtask}/**/*.js`, {interval: 500}, [`scripts:${subtask}`]);
 		}
 		gulp.watch(mainConfig.reload.args.files).on('change', $.livereload.changed);
-		gulp.watch(mainConfig.svg.args.src, ['svg']);
+		gulp.watch(mainConfig.svg.args.src, {interval: 500}, ['svg']);
 	};
 };
