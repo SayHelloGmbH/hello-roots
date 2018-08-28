964297d13fb095de508f04cd81995d8604147a47
nico_martin <mail@nicomartin.ch>
2018-08-28 23:06:19 +0200

    offline and 500 page

---
+++ offline.php
@@ -0,0 +1,15 @@
+<?php
+get_header();
+?>
+	<article class="page-section page-section--content">
+		<header class="content-section content-section--title">
+			<h1><?php echo __( 'Offliner', 'sht' ); ?></h1>
+		</header>
+		<div class="content-section post-content">
+			<?php
+			echo '<p><b>' . __( 'You\'re offline.', 'sht' ) . '</b></p>';
+			?>
+		</div>
+	</article>
+<?php
+get_footer();
