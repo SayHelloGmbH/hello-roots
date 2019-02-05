fef53dceb140bca2e83f4643db2a0caf75bd6601
Mark Howells-Mead <permanent.tourist@gmail.com>
2019-02-05 11:15:12 +0100

    Add telephoneUrl helper function

--- Classes/Package/Helpers.php
+++ Classes/Package/Helpers.php
@@ -39,5 +39,27 @@ class Helpers {
 
 		return $image_object->get_image($background);
 	}
+	
+		/**
+	 * Convert a telephone number in the common Swiss number formats to a valid tel: link
+	 * @param  string $number The legible phone number
+	 * @return string         The usable tel: link
+	 */
+	public static function telephoneUrl($number)
+	{
+		$nationalprefix = '+41';
+		$protocol = 'tel:';
+		$formattedNumber = $number;
+		if ($formattedNumber !== '') {
+			// add national dialing code prefix to tel: link if it's not already set
+			if (strpos($formattedNumber, '00') !== 0 && strpos($formattedNumber, '0800') !== 0 && strpos($formattedNumber, '+') !== 0 && strpos($formattedNumber, $nationalprefix) !== 0) {
+				$formattedNumber = preg_replace('/^0/', $nationalprefix, $formattedNumber);
+			}
+		}
+		$formattedNumber = str_replace('(0)', '', $formattedNumber);
+		$formattedNumber = preg_replace('~[^0-9\+]~', '', $formattedNumber);
+		$formattedNumber = trim($formattedNumber);
+		return $protocol . $formattedNumber;
+	}
 
 }
