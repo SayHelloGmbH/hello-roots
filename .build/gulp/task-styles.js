6ef775facec97afb2217e299c6b86f5f191f5e82
nico_martin <mail@nicomartin.ch>
2019-05-09 21:22:07 +0200

    added sass source maps

--- .build/gulp/task-styles.js
+++ .build/gulp/task-styles.js
@@ -6,13 +6,18 @@ import sassImportJson from 'gulp-sass-import-json';
 import autoprefixer from 'gulp-autoprefixer';
 import rename from 'gulp-rename';
 import livereload from 'gulp-livereload';
+import sourcemaps from 'gulp-sourcemaps';
 
 export const task = config => {
 	return gulp.src(config.assetsBuild + 'styles/**/*.scss')
 		.pipe(sassImportJson({isScss: true}))
+		.pipe(sourcemaps.init())
 		.pipe(sass().on('error', sass.logError))
+		.pipe(sourcemaps.write({includeContent: false}))
+		.pipe(sourcemaps.init({loadMaps: true}))
 		.pipe(autoprefixer())
 		.pipe(gulp.dest(config.assetsDir + 'styles/'))
+		.pipe(sourcemaps.write('.'))
 		.on('error', config.errorLog)
 		// minify
 		.pipe(minify())
