25e0d29ed24979610c1ab2ab5dccb02bacf21729
Mark Howells-Mead <permanent.tourist@gmail.com>
2019-06-19 15:12:47 +0200

    Switch to gulp-clean-css

--- .build/gulp/task-styles.js
+++ .build/gulp/task-styles.js
@@ -1,6 +1,6 @@
 import gulp from 'gulp';
 
-import minify from 'gulp-minify-css';
+import cleanCSS from 'gulp-clean-css';
 import sass from 'gulp-sass';
 import sassImportJson from 'gulp-sass-import-json';
 import autoprefixer from 'gulp-autoprefixer';
@@ -10,17 +10,17 @@ import sourcemaps from 'gulp-sourcemaps';
 
 export const task = config => {
 	return gulp.src(config.assetsBuild + 'styles/**/*.scss')
-		.pipe(sassImportJson({isScss: true}))
+		.pipe(sassImportJson({ isScss: true }))
 		.pipe(sourcemaps.init())
 		.pipe(sass().on('error', sass.logError))
-		.pipe(sourcemaps.write({includeContent: false}))
-		.pipe(sourcemaps.init({loadMaps: true}))
+		.pipe(sourcemaps.write({ includeContent: false }))
+		.pipe(sourcemaps.init({ loadMaps: true }))
 		.pipe(autoprefixer())
 		.pipe(gulp.dest(config.assetsDir + 'styles/'))
 		.pipe(sourcemaps.write('.'))
 		.on('error', config.errorLog)
 		// minify
-		.pipe(minify())
+		.pipe(cleanCSS({ compatibility: 'ie11' }))
 		.pipe(rename({
 			suffix: '.min'
 		}))
