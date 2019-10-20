2a3ee51870cf067b88e2c209188ce8a6887754b1
Mark Howells-Mead <permanent.tourist@gmail.com>
2019-10-20 15:56:41 +0200

    Use “masthead” component name

--- header.php
+++ header.php
@@ -11,27 +11,29 @@
 <section class="c-page__section" id="header">
 	<header class="c-page__header c-masthead" role="banner">
 
-		<h1 class="c-site__title">
-			<a class="c-site__titlelink" href="<?php echo get_home_url();?>"><?php echo get_bloginfo('name');?></a>
-		</h1>
+		<div class="c-masthead__inner">
+			<h1 class="c-site__title">
+				<a class="c-site__titlelink" href="<?php echo get_home_url();?>"><?php echo get_bloginfo('name');?></a>
+			</h1>
 
-		<?php
-		wp_nav_menu(
-			[
-				'theme_location' => 'primary',
-				'container'      => 'nav',
-				'container_class' => 'c-menu c-menu--primary',
-				'menu_id'        => 'primary-menu',
-				'menu_class'     => 'c-menu c-menu--primary',
-			]
-		);
-		?>
+			<?php
+			wp_nav_menu(
+				[
+					'theme_location' => 'primary',
+					'container'      => 'nav',
+					'container_class' => 'c-menu c-menu--primary',
+					'menu_id'        => 'primary-menu',
+					'menu_class'     => 'c-menu c-menu--primary',
+				]
+			);
+			?>
 
-		<button class="c-masthead__toggler o-menutoggler" aria-controls="mobile-menu" aria-expanded="false">
-			<span class="o-menutoggler__line"></span>
-			<span class="o-menutoggler__line"></span>
-			<span class="o-menutoggler__line"></span>
-		</button>
+			<button class="c-masthead__toggler o-menutoggler" aria-controls="mobile-menu" aria-expanded="false">
+				<span class="o-menutoggler__line"></span>
+				<span class="o-menutoggler__line"></span>
+				<span class="o-menutoggler__line"></span>
+			</button>
+		</div>
 
 	</header>
 </section>
