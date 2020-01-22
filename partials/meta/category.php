dae6d83d28fbbacd10beaea91bcd924d815fce80
Mark Howells-Mead <permanent.tourist@gmail.com>
2020-01-22 17:37:20 +0100

    Fix post tag and category output in single view

---
+++ partials/meta/category.php
@@ -0,0 +1,16 @@
+<?php
+if (!empty(get_the_terms(get_the_ID(), 'category'))) :
+	?>
+	<div class="c-taxonomy c-taxonomy--category">
+		<h3 class="c-taxonomy__title"><?php _ex('Categories', 'Taxonomy title (singular view)', 'sht');?></h3>
+		<?php
+		the_terms(
+			get_the_ID(),
+			'category',
+			'<ul class="c-taxonomy__entries c-taxonomy__entries--category"><li class="c-taxonomy__entry c-taxonomy__entry--category">',
+			'</li><li class="c-taxonomy__entry c-taxonomy__entry--category">',
+			'</li></ul>'
+		);
+		?>
+	</div>
+<?php endif;
