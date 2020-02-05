31970fe1da8a68ea167b6b23ac6c9d3000ba7d71
Mark Howells-Mead <permanent.tourist@gmail.com>
2020-02-05 11:29:01 +0100

    Remove column preview workaround (no longer broken)

--- .build/assets/gutenberg/blocks.js
+++ .build/assets/gutenberg/blocks.js
@@ -7,27 +7,6 @@ window.onload = function () {
 };
 
 wp.domReady(() => {
-
-	// Temporary workaround for broken preview Fn
-	// Only add once per block type!
-	// mhm 15.6.2019
-	var el = wp.element.createElement;
-	var allowColumnStyle = wp.compose.createHigherOrderComponent(function (BlockEdit) {
-		return function (props) {
-			var content = el(BlockEdit, props);
-
-			if(props.name === 'core/cover' && typeof props.insertBlocksAfter === 'undefined') {
-				content = el('div', {});
-			}
-
-			return el(
-				wp.element.Fragment, {}, content
-			);
-		};
-	}, 'allowColumnStyle');
-
-	wp.hooks.addFilter('editor.BlockEdit', 'my/gutenberg', allowColumnStyle);
-
 	wp.blocks.registerBlockStyle('core/cover', {
 		name: 'aspect-21',
 		label: '2:1'
