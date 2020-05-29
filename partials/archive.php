c14fe571b04accee6e5e5e02a0610abe0675f79c
Mark Howells-Mead <permanent.tourist@gmail.com>
2020-05-29 16:36:20 +0200

    Archive view

--- partials/archive.php
+++ partials/archive.php
@@ -1,12 +1,12 @@
 <section class="c-archive">
 
-	<div class="c-archive__content">
+	<div class="c-archive__content c-constraint">
 
 		<header class="c-archive__header">
 			<?php the_archive_title('<h1 class="c-archive__title">', '</h1>'); ?>
 		</header>
 
-		<div class="c-archive__content">
+		<div class="c-archive__entries">
 			<?php
 			if (have_posts()) {
 				while (have_posts()) {
