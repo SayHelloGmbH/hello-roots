2f40dbd644a98cf8677f8fc05a3401d7cd772e38
Mark Howells-Mead <permanent.tourist@gmail.com>
2020-05-29 17:02:03 +0200

    Tabs FTW

--- index.php
+++ index.php
@@ -1,21 +1,3 @@
 <?php
 
-get_header();
-if (have_posts()) {
-	?>
-	<section class="c-archive c-content">
-
-		<div class="c-archive__content c-content__content">
-			<?php
-			while (have_posts()) {
-				the_post();
-				get_template_part('partials/excerpt', get_post_type());
-			}
-			?>
-		</div>
-	</section>
-	<?php
-} else {
-	get_template_part('partials/singular', 'none');
-}
-get_footer();
+include_once 'archive.php';
