baad8d47b8ab2523a969aeec24dc4e1ee9661c66
Mark Howells-Mead <permanent.tourist@gmail.com>
2020-08-14 11:25:01 +0200

    Re-locate block example file

--- assets/gutenberg/blocks.asset.php
+++ assets/gutenberg/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-edit-post', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '63b5a893a0dceef40cd9381b8c136639');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-edit-post', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '6a338750f56ca3bbab3528313aab597c');
