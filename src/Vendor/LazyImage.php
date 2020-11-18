8341a130c524a119df9464fd9b5a8477e1509808
Mark Howells-Mead <permanent.tourist@gmail.com>
2020-11-18 12:49:57 +0100

    Array short syntax

--- src/Vendor/LazyImage.php
+++ src/Vendor/LazyImage.php
@@ -331,16 +331,16 @@ class LazyImage
 		$sizes = [];
 
 		foreach (get_intermediate_image_sizes() as $_size) {
-			if (in_array($_size, array( 'thumbnail', 'medium', 'medium_large', 'large' ))) {
+			if (in_array($_size, [ 'thumbnail', 'medium', 'medium_large', 'large' ])) {
 				$sizes[ $_size ][ 'width' ]  = get_option("{$_size}_size_w");
 				$sizes[ $_size ][ 'height' ] = get_option("{$_size}_size_h");
 				$sizes[ $_size ][ 'crop' ]   = (bool)get_option("{$_size}_crop");
 			} elseif (isset($_wp_additional_image_sizes[ $_size ])) {
-				$sizes[ $_size ] = array(
+				$sizes[ $_size ] = [
 					'width'  => $_wp_additional_image_sizes[ $_size ][ 'width' ],
 					'height' => $_wp_additional_image_sizes[ $_size ][ 'height' ],
 					'crop'   => $_wp_additional_image_sizes[ $_size ][ 'crop' ],
-				);
+				];
 			}
 		}
 
