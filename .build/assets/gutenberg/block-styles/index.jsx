5a1819c563aca813ff491bf3c416cc612ab50670
Mark Howells-Mead <permanent.tourist@gmail.com>
2020-11-18 13:30:44 +0100

    Add typographic settings

--- .build/assets/gutenberg/block-styles/index.jsx
+++ .build/assets/gutenberg/block-styles/index.jsx
@@ -2,8 +2,16 @@ import domReady from '@wordpress/dom-ready';
 import { registerBlockStyle } from '@wordpress/blocks';
 
 domReady(() => {
-	// registerBlockStyle('core/cover', {
-	// 	name: 'aspect-21',
-	// 	label: '2:1'
-	// });
+	registerBlockStyle('core/heading', {
+		name: 'small-text',
+		label: 'Klein'
+	});
+	registerBlockStyle('core/heading', {
+		name: 'large-text',
+		label: 'Gross'
+	});
+	registerBlockStyle('core/heading', {
+		name: 'xlarge-text',
+		label: 'Extra-gross'
+	});
 });
