cf34d02248fe07aa4ea30de785597ecf5ab8beef
Mark Howells-Mead <permanent.tourist@gmail.com>
2020-11-18 12:49:51 +0100

    Remove unnecessary Page PT function

--- src/PostType/Page.php
+++ src/PostType/Page.php
@@ -9,24 +9,7 @@ namespace SayHello\Theme\PostType;
  */
 class Page
 {
-
-	const POST_TYPE = 'page';
-	const PREFIX = 'sht_page';
-
 	public function run()
 	{
-		add_action('init', [$this, 'registerMetaFields']);
-	}
-
-	public function registerMetaFields()
-	{
-		register_post_meta(self::POST_TYPE, 'hide_title', [
-			'show_in_rest' => true,
-			'single' => true,
-			'type' => 'boolean',
-			'auth_callback' => function () {
-				return current_user_can('edit_pages');
-			}
-		]);
 	}
 }
