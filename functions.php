adb20659a1186267fb71f928fdcb625c96661fe9
Mark Howells-Mead <permanent.tourist@gmail.com>
2021-01-25 10:39:59 +0100

    Require PHP 8

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This is a starter theme by Say Hello
  * This file links the class file for the Theme and through it, any additional classes.
@@ -8,7 +9,7 @@
  * Namespace:   HelloTheme
  * Prefix:      sht
  *
- * Check if WordPress 5.0 and PHP 7.0 or newer and ACF is active
+ * Check if WordPress 5.0 and PHP 8.0 or newer and ACF is active
  */
 
 if (!defined('DISALLOW_FILE_EDIT')) {
@@ -19,19 +20,19 @@ if (!defined('DISALLOW_FILE_EDIT')) {
 if (!function_exists('dump')) {
 	function dump($var, $exit = false)
 	{
-		echo '<pre>'.print_r($var, true).'</pre>';
+		echo '<pre>' . print_r($var, true) . '</pre>';
 		if ($exit) {
 			exit;
 		}
 	}
 }
 
-if (version_compare(get_bloginfo('version'), '5.0', '<') || version_compare(PHP_VERSION, '7.0', '<') || ! class_exists('acf')) {
+if (version_compare(get_bloginfo('version'), '5.0', '<') || version_compare(PHP_VERSION, '8.0', '<') || !class_exists('acf')) {
 	add_action(
 		'admin_notices',
 		function () {
 			// translators: Admin notice for system requirements
-			echo '<div class="error"><p>' . sprintf(__('Dieses Theme benötigt PHP %1$s (oder neuer), WordPress %2$s (oder neuer) und das Plugin «Advanced Custom Fields». Ihre Website verfügt über PHP %3$s und WordPress %4$s. Bitte aktualisieren Sie die Abhängigkeiten. Das Theme wurde automatisch deaktiviert.', 'sha'), '7.0', '5.0', PHP_VERSION, $GLOBALS[ 'wp_version' ]) . '</p></div>';
+			echo '<div class="error"><p>' . sprintf(__('Dieses Theme benötigt PHP %1$s (oder neuer), WordPress %2$s (oder neuer) und das Plugin «Advanced Custom Fields». Ihre Website verfügt über PHP %3$s und WordPress %4$s. Bitte aktualisieren Sie die Abhängigkeiten. Das Theme wurde automatisch deaktiviert.', 'sha'), '7.0', '5.0', PHP_VERSION, $GLOBALS['wp_version']) . '</p></div>';
 		}
 	);
 
@@ -50,7 +51,7 @@ if (version_compare(get_bloginfo('version'), '5.0', '<') || version_compare(PHP_
 	 * See http://www.php-fig.org/psr/psr-4/ for an explanation of the file structure
 	 * and https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-4-autoloader-examples.md for usage examples.
 	 */
-	 spl_autoload_register(function ($class) {
+	spl_autoload_register(function ($class) {
 
 		// project-specific namespace prefix
 		$prefix = 'SayHello\\Theme\\';
@@ -68,23 +69,23 @@ if (version_compare(get_bloginfo('version'), '5.0', '<') || version_compare(PHP_
 		// get the relative class name
 		$relative_class = substr($class, $len);
 
-		 // replace the namespace prefix with the base directory, replace namespace
-		 // separators with directory separators in the relative class name, append
-		 // with .php
+		// replace the namespace prefix with the base directory, replace namespace
+		// separators with directory separators in the relative class name, append
+		// with .php
 		$file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
 
 		// if the file exists, require it
 		if (file_exists($file)) {
 			require $file;
 		}
-	 });
+	});
 
 	/**
 	 * Returns the Theme Instance
 	 *
 	 * @return Object Theme Object
 	 */
-	if (! function_exists('sht_theme')) {
+	if (!function_exists('sht_theme')) {
 		function sht_theme()
 		{
 			return SayHello\Theme\Theme::getInstance();
