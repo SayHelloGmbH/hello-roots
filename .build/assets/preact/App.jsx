54cad3a57a6ab63c014320dde0d1fe9514d9be31
Nico Martin <mail@nicomartin.ch>
2021-01-28 10:20:20 +0100

    added error + loading state

--- .build/assets/preact/App.jsx
+++ .build/assets/preact/App.jsx
@@ -5,20 +5,35 @@ const App = () => {
 	const [page, setPage] = React.useState(1);
 	const [pageSize, setPageSize] = React.useState(20);
 	const [data, setData] = React.useState([]);
+	const [loading, setLoading] = React.useState(false);
+	const [error, setError] = React.useState('');
 
 	const query = React.useMemo(() => `page=${page}&pageSize=${pageSize}`, [page, pageSize]);
 
 	React.useEffect(() => {
+		setLoading(true);
+		setError('');
 		fetch(`myendpoint?${query}`)
 			.then(resp => resp.json())
-			.then(data => setData(data));
+			.then(data => setData(data))
+			.catch(() => setError('Something went wrong'))
+			.finally(() => setLoading(false));
 	}, [query]);
 
+	if (error !== '') {
+		return <p>{error}</p>
+	}
+
+	if (loading) {
+		return <p>loading..</p>;
+	}
+
 	return <div>
-		<ul>
-			{data.map(bird => <li>{bird.name}</li>)}
-		</ul>
-		Page: {[1, 2, 3, 4, 5].map(p => <button onClick={() => setPage(p)}>{p}</button>)}<br />
+		{error ? <p>{error}</p> : loading ? <p>loading..</p> :
+			<ul>
+				{data.map(bird => <li>{bird.name}</li>)}
+			</ul>}
+		Page: {[1, 2, 3, 4, 5].map(p => <button onClick={() => setPage(p)}>{p}</button>)}<br/>
 		PageSize: {[10, 20, 30, 40, 50].map(p => <button onClick={() => setPageSize(p)}>{p}</button>)}
 	</div>
 }
