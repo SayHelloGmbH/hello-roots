84175471bfb30e296a3295e3e6a59f20294d14d4
Nico Martin <mail@nicomartin.ch>
2021-01-28 10:05:48 +0100

    added data fetch

--- .build/assets/preact/App.jsx
+++ .build/assets/preact/App.jsx
@@ -2,29 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-	const [liked, setLiked] = React.useState(false);
+	const [page, setPage] = React.useState(1);
+	const [pageSize, setPageSize] = React.useState(20);
+	const [data, setData] = React.useState([]);
+
+	const query = React.useMemo(() => `page=${page}&pageSize=${pageSize}`, [page, pageSize]);
 
 	React.useEffect(() => {
-
-		}, []
-	);
+		fetch(`myendpoint?${query}`)
+			.then(resp => resp.json())
+			.then(data => setData(data));
+	}, [query]);
 
 	return <div>
-		<button className={`button ${liked ? 'button--liked' : ''}`} disabled={liked}
-				style={{backgroundColor: liked ? 'pink' : 'blue'}} onClick={() => {
-			if (liked) {
-				alert('already liked');
-			}
-			$.get({
-				url: 'https://wordpress.hello/wp-json/wp/v2/',
-				success: () => {
-					setLiked(true);
-				},
-			});
-		}}>
-			like
-		</button>
-
+		<ul>
+			{data.map(bird => <li>{bird.name}</li>)}
+		</ul>
+		Page: {[1, 2, 3, 4, 5].map(p => <button onClick={() => setPage(p)}>{p}</button>)}<br />
+		PageSize: {[10, 20, 30, 40, 50].map(p => <button onClick={() => setPageSize(p)}>{p}</button>)}
 	</div>
 }
 
