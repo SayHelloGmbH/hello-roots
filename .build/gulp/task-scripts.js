4fd3185add491a31afe923219a11c64227ea1f90
Nico Martin <mail@nicomartin.ch>
2021-05-03 23:03:46 +0200

    switched to vynil-named for webpack entry point

--- .build/gulp/task-scripts.js
+++ .build/gulp/task-scripts.js
@@ -1,35 +1,27 @@
 import gulp from 'gulp';
 
 import gulpWebpack from 'webpack-stream';
+import named from 'vinyl-named';
+
 import livereload from 'gulp-livereload';
 import rename from 'gulp-rename';
 import uglify from 'gulp-uglify';
 import fs from 'fs';
 
-import babelloader from 'babel-loader';
-
-function getDirectories(path) {
-    return fs.readdirSync(path).filter(function (file) {
-        return fs.statSync(path + '/' + file).isDirectory();
-    });
-}
+const getDirectories = path =>
+    fs.readdirSync(path).filter(file => fs.statSync(path + '/' + file).isDirectory());
 
 export const task = config => {
     return new Promise(resolve => {
-        const bundles = getDirectories(`${config.assetsBuild}scripts/`);
-        const entry = {};
-        bundles.forEach(bundle => {
-            const filePath = `${config.assetsBuild}scripts/${bundle}/index.js`;
-            if (fs.existsSync(filePath)) {
-                entry[bundle] = './' + filePath;
-            }
-        });
-
-        gulp.src([`${config.assetsBuild}scripts/*`])
+        gulp.src(
+            getDirectories(`${config.assetsBuild}scripts/`).map(
+                bundle => `${config.assetsBuild}scripts/${bundle}.js`
+            )
+        )
+            .pipe(named())
             // Webpack
             .pipe(
                 gulpWebpack({
-                    entry,
                     mode: 'production',
                     module: {
                         rules: [
