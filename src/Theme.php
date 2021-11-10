08468e4a9b89834d2f661a0a4bff9e7820bbde29
Mark Howells-Mead <permanent.tourist@gmail.com>
2021-11-10 19:18:01 +0100

    Allow emojis

--- src/Theme.php
+++ src/Theme.php
@@ -198,8 +198,6 @@ class Theme
 		remove_action('wp_head', 'wp_generator');
 		remove_action('wp_head', 'wlwmanifest_link');
 		remove_action('wp_head', 'rsd_link');
-		remove_action('wp_head', 'print_emoji_detection_script', 7);
-		remove_action('wp_print_styles', 'print_emoji_styles');
 	}
 
 	/**
