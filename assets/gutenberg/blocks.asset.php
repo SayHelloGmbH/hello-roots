29f47a2146a90858b898c5f5ff5f6570d3463494
Mark Howells-Mead <permanent.tourist@gmail.com>
2022-02-07 18:00:23 +0100

    Recompiled assets

--- assets/gutenberg/blocks.asset.php
+++ assets/gutenberg/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => 'fbb882fda587a8edf86a3a7adc51e103');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => 'b637a5f5e1ffdba56f3f517588b7a2cb');
