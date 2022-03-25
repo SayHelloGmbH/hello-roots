66067d641de409cc7acc1c21f965c92f41e94845
Mark Howells-Mead <permanent.tourist@gmail.com>
2022-03-25 15:54:53 +0100

    Move polyfills to polyfill file

--- .build/assets/scripts/ui/_polyfill.js
+++ .build/assets/scripts/ui/_polyfill.js
@@ -23,3 +23,33 @@ if (window.Element && !Element.prototype.closest) {
         return el;
     };
 }
+
+// Load script if CSS custom properties are not supported natively
+if (!CSS.supports || !CSS.supports('(--foo: bar)')) {
+    let script = document.createElement('script');
+    script.setAttribute(
+        'src',
+        `${sht_theme.directory_uri}/assets/scripts/cssvars.min.js?version=${sht_theme.version}`
+    );
+    document.head.appendChild(script);
+}
+
+// Load script if object-fit is not supported natively
+if (!CSS.supports || !CSS.supports('object-fit', 'cover')) {
+    let script = document.createElement('script');
+    script.setAttribute(
+        'src',
+        `${sht_theme.directory_uri}/assets/scripts/object-fit.min.js?version=${sht_theme.version}`
+    );
+    document.head.appendChild(script);
+}
+
+// Load script if dvh is not supported natively
+if (!CSS.supports || !CSS.supports('height', '1dvh')) {
+    let script = document.createElement('script');
+    script.setAttribute(
+        'src',
+        `${sht_theme.directory_uri}/assets/scripts/vh.min.js?version=${sht_theme.version}`
+    );
+    document.head.appendChild(script);
+}
--- .build/assets/scripts/ui/index.js
+++ .build/assets/scripts/ui/index.js
@@ -12,33 +12,3 @@ console.log('%cDeveloped by', 'font-style: italic; font-size: 12px;');
 console.log('%cSay Hello GmbH', 'font-weight: bold; color: #000; font-size: 16px;');
 console.log('%chttps://sayhello.ch', 'color: #000; font-size: 12px;');
 console.log('---');
-
-// Load script if CSS custom properties are not supported natively
-if (!CSS.supports || !CSS.supports('(--foo: bar)')) {
-    let script = document.createElement('script');
-    script.setAttribute(
-        'src',
-        `${sht_theme.directory_uri}/assets/scripts/cssvars.min.js?version=${sht_theme.version}`
-    );
-    document.head.appendChild(script);
-}
-
-// Load script if object-fit is not supported natively
-if (!CSS.supports || !CSS.supports('object-fit', 'cover')) {
-    let script = document.createElement('script');
-    script.setAttribute(
-        'src',
-        `${sht_theme.directory_uri}/assets/scripts/object-fit.min.js?version=${sht_theme.version}`
-    );
-    document.head.appendChild(script);
-}
-
-// Load script if dvh is not supported natively
-if (!CSS.supports || !CSS.supports('height', '1dvh')) {
-    let script = document.createElement('script');
-    script.setAttribute(
-        'src',
-        `${sht_theme.directory_uri}/assets/scripts/vh.min.js?version=${sht_theme.version}`
-    );
-    document.head.appendChild(script);
-}
